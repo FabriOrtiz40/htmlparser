5556f48ddca47d7f1abca32e8f32fa17fd09ac10
FabriOrtiz40 <83090950+FabriOrtiz40@users.noreply.github.com>
2026-01-05 14:51:27 -0300

    Extract href attributes from anchor tags in Parse
    Updated the extractLinks function to collect href attributes from <a> elements when parsing HTML. This enables the Parse function to return actual links found in the HTML input.

--- link/link.go
+++ link/link.go
@@ -2,6 +2,7 @@ package link
 
 import (
 	"io"
+
 	"golang.org/x/net/html"
 )
 
@@ -12,7 +13,7 @@ type Link struct {
 
 // Recibe HTML desde cualquier fuente y parsea. Devuelve todos los links encontrados
 func Parse(r io.Reader) ([]Link, error) {
-		// 1. Parsear el HTML
+	// 1. Parsear el HTML
 	doc, err := html.Parse(r)
 	if err != nil {
 		return nil, err
@@ -25,12 +26,28 @@ func Parse(r io.Reader) ([]Link, error) {
 }
 
 // El Parse usa esta funcion para extraer todos los Links
-func extractLinks(n *html.Node, links *[]Link){
+func extractLinks(n *html.Node, links *[]Link) {
 	if n == nil {
 		return
 	}
 
 	// Acá después vamos a hacer cosas con el nodo n
+	if n.Type == html.ElementNode && n.Data == "a" {
+		var href string
+
+		for _, attr := range n.Attr {
+			if attr.Key == "href" {
+				href = attr.Val
+				break
+			}
+		}
+
+		link := Link{
+			Href: href,
+		}
+
+		*links = append(*links, link)
+	}
 
 	// DFS: primer hijo
 	extractLinks(n.FirstChild, links)
